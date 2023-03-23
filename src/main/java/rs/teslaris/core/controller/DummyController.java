4eda1a720b2e628e2377e762ff100b2185522c65
Ivan R. Mršulja <nighteliteace@gmail.com>
2023-03-23 12:59:22 +0100

    Feature/project setup (#9)
    * Initial project setup.
    
    * Installation instructions.
    
    * Delete .idea directory
    
    * Updated .gitignore to exclude .idea directory. Updated ApplicationStarter to include all modules when starting application.
    
    * Reconfigured project so it uses packages insted of modules. Updated installation instructions in readme. Refactored .gitignore so it has the reccomended configuration for Intellij Spring Boot developement.
    
    * Updated .gitignore.
    
    * Deleted .idea and *.iml
    
    * Update checkstyle.xml
    
    Co-authored-by: Dragan Ivanovic <chenejac@uns.ac.rs>
    
    * Delete target directory
    
    * Renamed packages, added installation instructions, added executable option when building .jar file. updated gitignore so it includes target folder.
    
    ---------
    
    Co-authored-by: Dragan Ivanovic <chenejac@uns.ac.rs>

---
+++ src/main/java/rs/teslaris/core/controller/DummyController.java
@@ -0,0 +1,15 @@
+package rs.teslaris.core.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dummy")
+public class DummyController {
+
+    @GetMapping
+    public String returnDummyData() {
+        return "TEST";
+    }
+}
---
+++ src/test/java/rs/teslaris/TeslarisTests.java
@@ -0,0 +1,13 @@
+package rs.teslaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeslarisTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
