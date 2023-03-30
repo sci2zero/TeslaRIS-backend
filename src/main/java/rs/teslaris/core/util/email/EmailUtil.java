742e2691d9301e5ae286c25059b1442b6b97fbbb
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2023-03-30 15:33:33 +0200

    Made noreply email address configurable from application.properties.

--- src/main/java/rs/teslaris/core/util/email/EmailUtil.java
+++ src/main/java/rs/teslaris/core/util/email/EmailUtil.java
@@ -1,6 +1,7 @@
 package rs.teslaris.core.util.email;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -12,10 +13,13 @@ public class EmailUtil {
 
     private final JavaMailSender mailSender;
 
+    @Value("${email.address}")
+    private String emailAddress;
+
     @Async
     public void sendSimpleEmail(String to, String subject, String text) {
         var message = new SimpleMailMessage();
-        message.setFrom("nekinasemail@email.com");
+        message.setFrom(emailAddress);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
