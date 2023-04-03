1cf6dee4e770b32835111413955ef09719a834dc
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2023-04-03 11:20:46 +0200

    Added repository annotation on RefreshTokenRepository.

--- src/main/java/rs/teslaris/core/repository/RefreshTokenRepository.java
+++ src/main/java/rs/teslaris/core/repository/RefreshTokenRepository.java
@@ -3,8 +3,10 @@ package rs.teslaris.core.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import rs.teslaris.core.model.RefreshToken;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
 
     @Query("select t from RefreshToken t where t.refreshTokenValue = :refreshTokenValue")
