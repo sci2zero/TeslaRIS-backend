12188d8f1fea6c3263ce2d14b45264108a1e308c
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2023-12-27 15:23:04 +0100

    Added initial CORS configuration.

--- src/main/java/rs/teslaris/core/configuration/BeanConfiguration.java
+++ src/main/java/rs/teslaris/core/configuration/BeanConfiguration.java
@@ -5,10 +5,13 @@ import com.google.common.cache.CacheBuilder;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.apache.tika.language.detect.LanguageDetector;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import rs.teslaris.core.util.exceptionhandling.exception.NotFoundException;
 
 @Configuration
@@ -36,5 +39,17 @@ public class BeanConfiguration {
             .maximumSize(10000)
             .expireAfterWrite(5, TimeUnit.SECONDS)
             .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173", "http://0.0.0.0:5173")
+                    .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH");
+            }
+        };
     }
 }
