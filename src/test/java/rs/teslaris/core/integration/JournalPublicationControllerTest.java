21c3d3f26019116eff738ff9bdb64822adb26f79
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2024-01-26 16:24:22 +0100

    Fixed tests.

--- src/test/java/rs/teslaris/core/integration/JournalPublicationControllerTest.java
+++ src/test/java/rs/teslaris/core/integration/JournalPublicationControllerTest.java
@@ -18,9 +18,9 @@ public class JournalPublicationControllerTest extends BaseTest {
     private ObjectMapper objectMapper;
 
     @Test
-    @WithMockUser(username = "author@author.com", password = "author")
+    @WithMockUser(username = "admin@admin.com", password = "admin")
     public void testFindMyPublicationsInJournal() throws Exception {
-        String jwtToken = authenticateResearcherAndGetToken();
+        String jwtToken = authenticateAdminAndGetToken();
 
         mockMvc.perform(MockMvcRequestBuilders.get(
                     "http://localhost:8081/api/journal-publication/journal/{journalId}/my-publications", 30)
@@ -30,9 +30,9 @@ public class JournalPublicationControllerTest extends BaseTest {
     }
 
     @Test
-    @WithMockUser(username = "author@author.com", password = "author")
+    @WithMockUser(username = "admin@admin.com", password = "admin")
     public void testFindPublicationsInJournal() throws Exception {
-        String jwtToken = authenticateResearcherAndGetToken();
+        String jwtToken = authenticateAdminAndGetToken();
 
         mockMvc.perform(MockMvcRequestBuilders.get(
                     "http://localhost:8081/api/journal-publication/journal/{journalId}/", 30)
