70f64668a9181ba43e55c7e832b7bb99d15ac0ea
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2024-06-10 10:57:38 +0200

    Added import notifications.

--- src/main/java/rs/teslaris/core/importer/controller/CommonHarvestController.java
+++ src/main/java/rs/teslaris/core/importer/controller/CommonHarvestController.java
@@ -1,6 +1,8 @@
 package rs.teslaris.core.importer.controller;
 
 import jakarta.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,19 +12,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import rs.teslaris.core.importer.service.interfaces.ScopusHarvester;
 import rs.teslaris.core.importer.utility.scopus.ScopusImportUtility;
+import rs.teslaris.core.service.interfaces.commontypes.NotificationService;
+import rs.teslaris.core.service.interfaces.user.UserService;
 import rs.teslaris.core.util.jwt.JwtUtil;
+import rs.teslaris.core.util.notificationhandling.NotificationFactory;
 
 @RestController
 @RequestMapping("/api/import-scopus")
 @RequiredArgsConstructor
-public class ScopusHarvestController {
+public class CommonHarvestController {
 
     private final JwtUtil tokenUtil;
 
     private final ScopusHarvester scopusHarvester;
 
+    private final NotificationService notificationService;
+
+    private final UserService userService;
+
     @Value("${scopus.api.key}")
     private String apiKey;
+
 
     @PostConstruct
     public void init() {
@@ -34,7 +44,25 @@ public class ScopusHarvestController {
         @RequestHeader("Authorization") String bearerToken, @RequestParam Integer startYear,
         @RequestParam Integer endYear) {
         var userId = tokenUtil.extractUserIdFromToken(bearerToken.split(" ")[1]);
+        var newEntriesCount = new HashMap<Integer, Integer>();
 
-        return scopusHarvester.harvestDocumentsForAuthor(userId, startYear, endYear);
+        var newDocumentImportCountByUser =
+            scopusHarvester.harvestDocumentsForAuthor(userId, startYear, endYear, newEntriesCount);
+
+        // TODO: All other harvesters are called here sequentially
+
+        newDocumentImportCountByUser.keySet().forEach(key -> {
+            if (Objects.equals(key, userId)) {
+                return;
+            }
+
+            var notificationValues = new HashMap<String, String>();
+            notificationValues.put("newImportCount", newDocumentImportCountByUser.get(key) + "");
+            notificationService.createNotification(
+                NotificationFactory.contructNewImportsNotification(notificationValues,
+                    userService.findOne(key)));
+        });
+
+        return newDocumentImportCountByUser.getOrDefault(userId, 0);
     }
 }
--- src/main/java/rs/teslaris/core/importer/service/interfaces/ScopusHarvester.java
+++ src/main/java/rs/teslaris/core/importer/service/interfaces/ScopusHarvester.java
@@ -1,9 +1,12 @@
 package rs.teslaris.core.importer.service.interfaces;
 
+import java.util.HashMap;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ScopusHarvester {
 
-    Integer harvestDocumentsForAuthor(Integer userId, Integer startYear, Integer endYear);
+    HashMap<Integer, Integer> harvestDocumentsForAuthor(Integer userId, Integer startYear,
+                                                        Integer endYear,
+                                                        HashMap<Integer, Integer> newEntriesCount);
 }
