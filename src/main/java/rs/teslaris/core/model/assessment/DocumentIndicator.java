8b656317210d2c794ef02a1dde4226c662c469a7
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2024-09-19 21:09:48 +0200

    Added assessment model.

--- src/main/java/rs/teslaris/core/model/assessment/DocumentIndicator.java
+++ src/main/java/rs/teslaris/core/model/assessment/DocumentIndicator.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
