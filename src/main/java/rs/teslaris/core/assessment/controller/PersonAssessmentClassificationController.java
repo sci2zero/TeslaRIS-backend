21275e201af023a417f7ddf92a7ee7f752ec59e3
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2025-02-17 18:22:09 +0100

    Small refactor.

--- src/main/java/rs/teslaris/core/assessment/controller/PersonAssessmentClassificationController.java
+++ src/main/java/rs/teslaris/core/assessment/controller/PersonAssessmentClassificationController.java
@@ -1,13 +1,11 @@
 package rs.teslaris.core.assessment.controller;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
