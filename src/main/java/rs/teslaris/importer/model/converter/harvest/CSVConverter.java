14070a6333158d24a9282bb92248def0c4a56702
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2025-06-11 19:11:13 +0200

    Added CSV format description support.

--- src/main/java/rs/teslaris/importer/model/converter/harvest/CSVConverter.java
+++ src/main/java/rs/teslaris/importer/model/converter/harvest/CSVConverter.java
@@ -3,6 +3,7 @@ package rs.teslaris.importer.model.converter.harvest;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import rs.teslaris.core.indexmodel.DocumentPublicationType;
 import rs.teslaris.core.model.document.DocumentContributionType;
 import rs.teslaris.core.util.FunctionalUtil;
@@ -16,6 +17,12 @@ import rs.teslaris.importer.model.common.PersonName;
 
 public class CSVConverter {
 
+    private static final Pattern orcidRegexPattern =
+        Pattern.compile("^\\d{4}-\\d{4}-\\d{4}-[\\dX]{4}$", Pattern.CASE_INSENSITIVE);
+
+    private static final Pattern scopusAuthorIdPattern =
+        Pattern.compile("^\\d+$", Pattern.CASE_INSENSITIVE);
+
     /**
      * Converts a row from a CSV containing publication metadata into a standard internal format.
      *
@@ -24,12 +31,12 @@ public class CSVConverter {
      * of data it holds, with anonymized examples:</p>
      *
      * <ul>
-     *   <li><b>[0]</b> Publication Type - "Article", "Conference paper"</li>
-     *   <li><b>[1]</b> Authors - Author name variants that they used in publication </li>
+     *   <li><b>[0]</b> *Publication Type - "Article", "Conference paper"</li>
+     *   <li><b>[1]</b> *Authors - Author name variants that they used in publication </li>
      *   <li><b>[2]</b> Author Full Names with Identifier - "Smith, John (12345678900); Doe, Jane (09876543210)"</li>
      *   <li><b>[3]</b> Author Full Names with Affiliations - "Smith J., University of Something, City; Jane D., University, City"</li>
-     *   <li><b>[4]</b> Title - "Integration of a system with an OAI-PMH compatible repository"</li>
-     *   <li><b>[5]</b> Year - "2012"</li>
+     *   <li><b>[4]</b> *Title - "Integration of a system with an OAI-PMH compatible repository"</li>
+     *   <li><b>[5]</b> *Year - "2012"</li>
      *   <li><b>[6]</b> Volume - "56"</li>
      *   <li><b>[7]</b> Issue - "2"</li>
      *   <li><b>[8]</b> Start Page - "104"</li>
@@ -39,77 +46,127 @@ public class CSVConverter {
      *   <li><b>[12]</b> Link / URL</li>
      *   <li><b>[13]</b> Abstract</li>
      *   <li><b>[14]</b> Author supplied keywords, separated by "; "</li>
-     *   <li><b>[15]</b> Conference name</li>
-     *   <li><b>[16]</b> Journal Name</li>
+     *   <li><b>[15]</b> *Conference name</li>
+     *   <li><b>[16]</b> *Journal Name</li>
      *   <li><b>[17]</b> ISSN</li>
      *   <li><b>[18]</b> ISBN</li>
      *   <li><b>[19]</b> ConfID</li>
-     *   <li><b>[20]</b> Electronic ID</li>
+     *   <li><b>[20]</b> *Electronic ID</li>
      * </ul>
      */
     public static Optional<DocumentImport> toCommonImportModel(String[] record) {
-        var document = new DocumentImport();
-
-        if (record[4].isBlank() || record[5].isBlank() || record[20].isBlank()) {
+        if (isMissingRequiredFields(record)) {
             return Optional.empty();
         }
 
+        var document = new DocumentImport();
+        populateBasicMetadata(document, record);
+
+        if (!populatePublicationTypeAndEvent(document, record)) {
+            return Optional.empty();
+        }
+
+        populateAuthors(document, record);
+        populatePagination(document, record);
+        populateIdentifiersAndDescriptions(document, record);
+
+        return Optional.of(document);
+    }
+
+    private static boolean isMissingRequiredFields(String[] record) {
+        return record[4].isBlank() || record[5].isBlank() || record[20].isBlank();
+    }
+
+    private static void populateBasicMetadata(DocumentImport document, String[] record) {
         document.getTitle().add(new MultilingualContent("EN", record[4], 1));
         document.setDocumentDate(record[5]);
         document.setIdentifier(record[20]);
+        document.setVolume(record[6]);
+        document.setIssue(record[7]);
+    }
 
-        if (record[0].equals("Article") && !record[16].isBlank()) {
+    private static boolean populatePublicationTypeAndEvent(DocumentImport document,
+                                                           String[] record) {
+        String type = record[0];
+
+        if ("Article".equals(type) && !record[16].isBlank()) {
             document.setPublicationType(DocumentPublicationType.JOURNAL_PUBLICATION);
             document.getPublishedIn().add(new MultilingualContent("EN", record[16], 1));
-        } else if (record[0].equals("Conference paper") && !record[15].isBlank()) {
+        } else if ("Conference paper".equals(type) && !record[15].isBlank()) {
             document.setPublicationType(DocumentPublicationType.PROCEEDINGS_PUBLICATION);
             document.getPublishedIn()
                 .add(new MultilingualContent("EN", "Proceedings of " + record[15], 1));
+
             var event = new Event();
             event.getName().add(new MultilingualContent("EN", record[15], 1));
             event.setConfId(record[19]);
-            event.setDateFrom(LocalDate.of(Integer.parseInt(record[5]), 1, 1));
-            event.setDateTo(LocalDate.of(Integer.parseInt(record[5]), 12, 31));
+
+            int year = Integer.parseInt(record[5]);
+            event.setDateFrom(LocalDate.of(year, 1, 1));
+            event.setDateTo(LocalDate.of(year, 12, 31));
+
             document.setEvent(event);
         } else {
-            return Optional.empty();
+            return false;
         }
 
-        var authorFullNamesWithIdentifiers = record[2].split("; ");
-        var authorFullNamesWithAffiliations = record[3].split("; ");
-        FunctionalUtil.forEachWithCounter(Arrays.asList(record[1].split("; ")), (i, authorName) -> {
+        return true;
+    }
+
+    private static void populateAuthors(DocumentImport document, String[] record) {
+        var identifiers = record[2].split("; ");
+        var affiliations = record[3].split("; ");
+        var names = record[1].split("; ");
+
+        FunctionalUtil.forEachWithCounter(Arrays.asList(names), (i, authorName) -> {
             var contribution = new PersonDocumentContribution();
             contribution.setOrderNumber(i + 1);
             contribution.setContributionType(DocumentContributionType.AUTHOR);
 
-            var person = new Person();
-            var authorNameParts = authorName.split(" ");
-            person.setName(new PersonName(authorNameParts[1], "", authorNameParts[0]));
-
-            if (i < authorFullNamesWithIdentifiers.length) {
-                var identifier = authorFullNamesWithIdentifiers[i].split("\\(")[1].replace(")", "");
-                person.setScopusAuthorId(identifier);
-                person.setImportId(identifier);
-            }
+            var person = createPerson(authorName, i < identifiers.length ? identifiers[i] : null);
             contribution.setPerson(person);
 
-            if (i < authorFullNamesWithAffiliations.length) {
-                var institution = new OrganisationUnit();
-                institution.setImportId(String.valueOf(i));
-                var institutionName = authorFullNamesWithAffiliations[i].split(", ", 2)[1];
-                institution.getName().add(new MultilingualContent("EN", institutionName, 1));
+            if (i < affiliations.length) {
+                var institution = createInstitution(i, affiliations[i]);
                 contribution.getInstitutions().add(institution);
             }
 
             document.getContributions().add(contribution);
         });
+    }
 
-        document.setVolume(record[6]);
-        document.setIssue(record[7]);
+    private static Person createPerson(String name, String identifierEntry) {
+        var parts = name.split(" ");
+        var person = new Person();
+        person.setName(new PersonName(parts[1], "", parts[0]));
 
+        if (identifierEntry != null && identifierEntry.contains("(")) {
+            String identifier = identifierEntry.split("\\(")[1].replace(")", "");
+            person.setImportId(identifier);
+
+            if (scopusAuthorIdPattern.matcher(identifier).matches()) {
+                person.setScopusAuthorId(identifier);
+            } else if (orcidRegexPattern.matcher(identifier).matches()) {
+                person.setOrcid(identifier);
+            }
+        }
+
+        return person;
+    }
+
+    private static OrganisationUnit createInstitution(int index, String affiliation) {
+        var institution = new OrganisationUnit();
+        institution.setImportId(String.valueOf(index));
+        var name = affiliation.split(", ", 2)[1];
+        institution.getName().add(new MultilingualContent("EN", name, 1));
+        return institution;
+    }
+
+    private static void populatePagination(DocumentImport document, String[] record) {
         if (record[10].isBlank()) {
             document.setStartPage(record[8]);
             document.setEndPage(record[9]);
+
             if (!record[8].isBlank() && !record[9].isBlank()) {
                 document.setNumberOfPages(
                     Integer.parseInt(record[9]) - Integer.parseInt(record[8]));
@@ -126,7 +183,10 @@ public class CSVConverter {
                 document.setEndPage("");
             }
         }
+    }
 
+    private static void populateIdentifiersAndDescriptions(DocumentImport document,
+                                                           String[] record) {
         document.setDoi(record[11]);
 
         if (!record[12].isBlank()) {
@@ -134,18 +194,17 @@ public class CSVConverter {
         }
 
         if (!record[17].isBlank()) {
-            if (!record[17].contains("-")) {
-                document.setEIssn(record[17].substring(0, 4) + "-" + record[17].substring(4));
-            } else {
-                document.setEIssn(record[17]);
-            }
+            document.setEIssn(formatIssn(record[17]));
         }
+
         document.setIsbn(record[18]);
 
         document.getDescription().add(new MultilingualContent("EN", record[13], 1));
         document.getKeywords()
             .add(new MultilingualContent("EN", record[14].replace("; ", "\n"), 1));
+    }
 
-        return Optional.of(document);
+    private static String formatIssn(String raw) {
+        return raw.contains("-") ? raw : raw.substring(0, 4) + "-" + raw.substring(4);
     }
 }
