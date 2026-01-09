8963efdd1600f9b56041f00ccc4093736fc2c96c
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2026-01-09 10:41:44 +0100

    Fixed failing CI test.

--- src/test/java/rs/teslaris/core/integration/exporter/RoCrateExportControllerTest.java
+++ src/test/java/rs/teslaris/core/integration/exporter/RoCrateExportControllerTest.java
@@ -2,16 +2,25 @@ package rs.teslaris.core.integration.exporter;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import rs.teslaris.core.dto.commontypes.ReindexRequestDTO;
+import rs.teslaris.core.indexmodel.EntityType;
 import rs.teslaris.core.integration.BaseTest;
 
 @SpringBootTest
 public class RoCrateExportControllerTest extends BaseTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @Test
     @WithMockUser(username = "test.admin@test.com", password = "testAdmin")
@@ -31,6 +40,17 @@ public class RoCrateExportControllerTest extends BaseTest {
     @WithMockUser(username = "test.admin@test.com", password = "testAdmin")
     public void testExportBibliographyRoCrate() throws Exception {
         String jwtToken = authenticateAdminAndGetToken();
+
+        var request = new ReindexRequestDTO(List.of(EntityType.PUBLICATION));
+        String requestBody = objectMapper.writeValueAsString(request);
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(
+                        "http://localhost:8081/api/reindex?reharvestCitationIndicators=false")
+                    .content(requestBody)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                    .header("Idempotency-Key", "MOCK_KEY_REINDEX2"))
+            .andExpect(status().isOk());
 
         mockMvc.perform(
                 MockMvcRequestBuilders.get(
