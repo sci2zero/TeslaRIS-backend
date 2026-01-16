6ff1238e78ca2f91dc82efd8d6179831b853e3ed
Ivan Mrsulja <mrsuljaivancic@gmail.com>
2026-01-16 09:07:04 +0100

    Enum constant fix.

--- src/main/java/rs/teslaris/assessment/model/indicator/ApplicableEntityType.java
+++ src/main/java/rs/teslaris/assessment/model/indicator/ApplicableEntityType.java
@@ -2,11 +2,11 @@ package rs.teslaris.assessment.model.indicator;
 
 public enum ApplicableEntityType {
     MONOGRAPH,
-    MONOGRAPH_PUBLICATION,
     EVENT,
     DOCUMENT,
     PERSON,
     ORGANISATION_UNIT,
     PUBLICATION_SERIES,
-    ALL
+    ALL,
+    MONOGRAPH_PUBLICATION
 }
