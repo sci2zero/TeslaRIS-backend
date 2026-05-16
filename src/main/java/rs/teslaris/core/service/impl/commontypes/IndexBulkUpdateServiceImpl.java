9d95d3d2f0dc467d8df9758ddd42e7f74a1dbf4f
Rade Pejanović <radepejanovic@gmail.com>
2026-05-16 18:18:13 +0200

    Added removal of a field from record based on id field value

--- src/main/java/rs/teslaris/core/service/impl/commontypes/IndexBulkUpdateServiceImpl.java
+++ src/main/java/rs/teslaris/core/service/impl/commontypes/IndexBulkUpdateServiceImpl.java
@@ -45,6 +45,36 @@ public class IndexBulkUpdateServiceImpl implements IndexBulkUpdateService {
     }
 
     @Override
+    public void removeIdFieldFromRecord(String indexName, String fieldMappingName, Integer queryValue,
+                                        String idField, Integer idToRemove) {
+        var request = new UpdateByQueryRequest.Builder()
+                .index(indexName)
+                .waitForCompletion(true)
+                .query(q -> q.bool(b -> b
+                        .must(m -> m.term(t -> t
+                                .field(fieldMappingName)
+                                .value(queryValue)))
+                        .must(m -> m.term(t -> t
+                                .field(idField)
+                                .value(idToRemove)))))
+                .script(s -> s
+                        .inline(i -> i
+                                .source("ctx._source.remove('" + idField + "')")
+                                .lang("painless")))
+                .build();
+
+        try {
+            elasticsearchClient.updateByQuery(request);
+            elasticsearchClient.indices().refresh(r -> r.index(indexName));
+
+            log.info("Removed field {} from document in index {} found by field {} with value {}",
+                    idField, indexName, fieldMappingName, queryValue);
+        } catch (Exception e) {
+            log.error("An error occurred while removing id field from index records: {}", e.getMessage());
+        }
+    }
+
+    @Override
     public void setIdFieldForRecord(String indexName, String fieldMappingName, Integer queryValue,
                                     String idField, Integer idToSet) {
         var request = Objects.nonNull(idToSet) ?
