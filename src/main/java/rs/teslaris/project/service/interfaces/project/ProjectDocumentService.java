87abf5069a73422d95250fff140e90c58c1fbb08
pejinovics <pejinovicstefan@gmail.com>
2026-08-07 11:33:36 +0200

    add project documents listing and external document support

--- src/main/java/rs/teslaris/project/service/interfaces/project/ProjectDocumentService.java
+++ src/main/java/rs/teslaris/project/service/interfaces/project/ProjectDocumentService.java
@@ -1,5 +1,6 @@
 package rs.teslaris.project.service.interfaces.project;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
 import rs.teslaris.core.service.interfaces.JPAService;
 import rs.teslaris.project.dto.project.ProjectDocumentDTO;
@@ -7,6 +8,8 @@ import rs.teslaris.project.model.project.ProjectDocument;
 
 @Service
 public interface ProjectDocumentService extends JPAService<ProjectDocument> {
+
+    List<ProjectDocumentDTO> readProjectDocuments(Integer projectId);
 
     ProjectDocument createProjectDocument(ProjectDocumentDTO projectDocumentDTO);
 
