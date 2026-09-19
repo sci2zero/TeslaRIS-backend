f5d461fc2a76c2c3f5f65de6389f7a06aa3072c5
pejinovics <pejinovicstefan@gmail.com>
2026-09-19 10:51:37 +0200

    Added project unbind endpoints

--- src/main/java/rs/teslaris/project/controller/project/ProjectController.java
+++ src/main/java/rs/teslaris/project/controller/project/ProjectController.java
@@ -10,15 +10,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import rs.teslaris.core.annotation.Idempotent;
+import rs.teslaris.core.service.interfaces.person.PersonService;
+import rs.teslaris.core.service.interfaces.user.UserService;
+import rs.teslaris.core.util.jwt.JwtUtil;
 import rs.teslaris.project.annotation.ProjectEditCheck;
 import rs.teslaris.project.dto.project.OrganisationUnitProjectContributionDTO;
 import rs.teslaris.project.dto.project.PersonProjectContributionDTO;
@@ -37,6 +42,12 @@ public class ProjectController {
     private final ProjectService projectService;
 
     private final ProjectCreationService projectCreationService;
+
+    private final JwtUtil tokenUtil;
+
+    private final PersonService personService;
+
+    private final UserService userService;
 
     @GetMapping("/{projectId}/can-edit")
     @PreAuthorize("hasAuthority('EDIT_PROJECTS')")
@@ -122,6 +133,32 @@ public class ProjectController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteProjects(@PathVariable Integer projectId) {
         projectService.deleteProject(projectId);
+    }
+
+    @PatchMapping("/{projectId}/unbind-researcher")
+    @PreAuthorize("hasAuthority('UNBIND_YOURSELF_FROM_PROJECT')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ProjectEditCheck
+    public void unbindResearcherFromProject(@PathVariable Integer projectId,
+                                            @RequestHeader(value = "Authorization")
+                                            String bearerToken) {
+        var userId = tokenUtil.extractUserIdFromToken(bearerToken);
+        var personId = personService.getPersonIdForUserId(userId);
+
+        projectService.unbindResearcherFromProject(personId, projectId);
+    }
+
+    @PatchMapping("/{projectId}/unbind-institution-researchers")
+    @PreAuthorize("hasAuthority('UNBIND_EMPLOYEES_FROM_PROJECT')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ProjectEditCheck
+    public void unbindInstitutionResearchersFromProject(@PathVariable Integer projectId,
+                                                        @RequestHeader(value = "Authorization")
+                                                        String bearerToken) {
+        var userId = tokenUtil.extractUserIdFromToken(bearerToken);
+        var institutionId = userService.getUserOrganisationUnitId(userId);
+
+        projectService.unbindInstitutionResearchersFromProject(institutionId, projectId);
     }
 
 
